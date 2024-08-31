bdabf4802ad8ced1e7ec61174742fe1900c9350d
Cupcake-Legend <s160422020@student.ubaya.ac.id>
2024-08-31 08:24:17 +0700

    Fix bug logout

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ Route::middleware('guest_')->group(function () {
 
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('login', [LoginController::class, 'login'])->name('login');
+    Route::post('logout', [LoginController::class, 'login'])->name('login');
 });
 
 // Route::get('/peserta/kotalama/{id}', [PesertaKotalamaController::class,'kotalamaData']);
 // Route::get('/penpos/kotalama', [PenposKotalamaController::class,'penposData']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
+
 
 //Route::view('/dashboard', "penpos.dashboard")->name('penpos_dashboard');
 //Route::view('/kotalama', "penpos.kotalama")->name('kotalama'); //bikin error soalnya cuma view jadi gaada variabel yang disiapin controller
