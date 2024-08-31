0e80824c08d215b437467793a373e26a6a6b96c1
WhoIsLiLY <127612137+WhoIsLiLY@users.noreply.github.com>
2024-08-31 08:43:51 +0700

    fix logout bug 2.0

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest_')->group(function () {
     Route::get('/', function () {
         return view('login');
-    });
+    })->name('/');
 
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('login', [LoginController::class, 'login'])->name('login');
-    Route::post('logout', [LoginController::class, 'login'])->name('login');
 });
 
 // Route::get('/peserta/kotalama/{id}', [PesertaKotalamaController::class,'kotalamaData']);
