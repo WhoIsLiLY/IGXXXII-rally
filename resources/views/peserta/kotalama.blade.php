2e3e70dfaf5dc0eadff7a665f2a9b95eed95a8b6
AlexanderKentSo <alexanderkentso96@gmail.com>
2024-09-12 20:31:30 +0700

    revisi layout kotalama

--- resources/views/peserta/kotalama.blade.php
+++ resources/views/peserta/kotalama.blade.php
@@ -10,7 +10,7 @@
     echo '</pre>';
     ?>
  --}}
-    <!-- NAVBAR - Scan Button and Logout -->
+    <!-- NAVBAR and Logout -->
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand text-main" href="#" style = "font-size:40px;">Industrial Games</a>
@@ -39,20 +39,19 @@
         .choice{
             border: 3px solid yellow;
         }
-        .container-box {
+        /* .container-box {
             display: flex;
             flex-wrap: wrap;
             justify-content: space-between;
-        }
+        } */
 
         .box {
             background-color: #E4AD49;
-            padding: 15px;
+            padding: 16px;
             border-radius: 10px;
             text-align: center;
             position: relative;
-            margin-bottom: 15px;
-            width: 100%;
+            margin-bottom: 10px;
         }
         @media (min-width: 768px) {
             .box {
@@ -85,8 +84,8 @@
 
         
         .city{
-            width: 200px;
-            height: 160px;
+            width: 150px;
+            height: 120px;
             font-size: 20px;
             text-align: center;
             position: absolute;
@@ -96,82 +95,107 @@
             z-index: 10; 
         }
 
+        .pointer {
+            position: absolute;
+            width: 0;
+            height: 0;
+            border-left: 50px solid transparent;
+            border-right: 50px solid transparent;
+            border-bottom: 100px solid #007bff;
+        }
     </style>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <div >
-    <div class="container mt-4" >
+    <div class="container mt-4">
         <div class="row  py-2 mb-2 col-6 " >
             <h1>{{$player->username}}</h1>
         </div>
-
-        <div class="container-box d-flex justify-content-between">
-            <div class="box">
-                <p><b>Passengers in Bus:</b> <span class="number">{{ $bus->passenger ?? 'N/A' }}</span></p>
-            </div>
-            <div class="box">
-                <p><b>Kotalama Passengers:</b> <span class="number">{{ $kotalama->total_passengers ?? '000' }}</span></p>
-            </div>
-            <div class="box">
-                <p><b>Total Duration:</b> <span class="number">{{ $kotalama->total_duration ?? 'N/A' }}</span></p>
-            </div>
-            <div class="box">
-                <p><b>Current Location:</b> <span class="number">{{ $cities[$kotalama->location_id]->name }}</span></p>
-            </div>
-            <div class="box">
-                <p><b>Remaining Fuel:</b> <span class="number">{{ $bus->fuel ?? 'N/A' }}</span></p>
-            </div>
-        </div>
-        <div class="container text-center">
-  
-</div>
-    </div>
-    <div class="container d-flex justify-content-center mt-4 ">
-        <div class="map-container">
-            <img src="{{ asset('img/kotaLama/Map Besar.png')}}" class="city-map">
-            <div class="city" id="1" style="top: 150px; left: 250px;"></div>
-            <div class="city"id="2" style="top: 70px; left: 650px;"></div>
-            <div class="city" id="3"style="top: 450px; left: 520px;"> </div>
-            <div class="city" id="4"style="top: 365px; left: 780px;"></div>
-            <div class="city"id="5" style="top: 310px; left: 190px;"></div>
-            <div class="city" id="6"style="top: 500px; left: 200px;"></div>
-            <div class="city" id="7"style="top: 150px; left: 870px;"></div>
-            <div class="city" id="8"style="top: 250px; left: 480px;"></div>
-            <div class="city" id="9"style="top: 550px; left: 950px;"></div>
-        </div>    
-    </div>
-    <div class="container d-flex justify-content-center mt-4">
-        <form action=""method="POST" class="d-inline">
-            @csrf
-            <button class="rounded btn btn-primary mb-4 " style="font-size:20px; padding:20px; width: 250px;">Restart</button>
-            <button class="rounded btn btn-primary mb-4" style="font-size:20px; padding:20px; width:250px">Save</button>
-        </form>               
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="moveModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Move City</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+        <div class="d-flex w-100" style="justify-content: space-between;">
+            <div class="w-24 h-100">
+                <div class="box" style="width: 100%;">
+                    <p><b>Current Location:</b> <span class="number">{{ $cities[$kotalama->location_id]->name }}</span></p>
                 </div>
-                <div class="modal-body">
-                    <p id="modalMessage"></p>
+                <div class="box" style="width: 100%;">
+                    <p><b>Remaining Fuel:</b> <span class="number">{{ $bus->fuel ?? 'N/A' }}</span></p>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="confirmMove">Confirm</button>
+                <div class="box" style="width: 100%;">
+                    <p><b>Passengers in Bus:</b> <span class="number">{{ $bus->passenger ?? 'N/A' }}</span></p>
+                </div>
+                <div class="box" style="width: 100%;">
+                    <p><b>Kotalama Passengers:</b> <span class="number">{{ $kotalama->total_passengers ?? '000' }}</span></p>
+                </div>
+                <div class="box" style="width: 100%;">
+                    <p><b>Total Duration:</b> <span class="number">{{ $kotalama->total_duration ?? 'N/A' }}</span></p>
                 </div>
             </div>
+            <div class="map-container" style="width: 85%; margin-top:-5%; position:relative;">
+                <img src="{{ asset('img/kotaLama/MapBesarPolos.png')}}" class="city-map">
+                {{-- <div class="pointer" id="1" style="top:0;left:0;">text</div> --}}
+                <div class="city" id="1" style="top: 90px; left: 175px;">Kota A</div>
+                <div class="city" id="2" style="top: 45px; left: 520px;">Kota B</div>
+                <div class="city" id="3" style="top: 370px; left: 420px;">Kota C</div>
+                <div class="city" id="4" style="top: 280px; left: 620px;">Kota D</div>
+                <div class="city" id="5" style="top: 250px; left: 130px;">Kota E</div>
+                <div class="city" id="6" style="top: 400px; left: 150px;">Kota F</div>
+                <div class="city" id="7" style="top: 140px; left: 700px;">Kota G</div>
+                <div class="city" id="8" style="top: 165px; left: 380px;">Pom Bensin</div>
+                <div class="city" id="9" style="top: 450px; left: 780px;">Kota Lama</div>
+                <form action=""method="POST" class="d-flex w-100 mt-3" style="justify-content: center;">
+                    @csrf
+                    <button class="rounded btn btn-primary" style="font-size:20px; padding:10px; width:200px; margin:0 20px;">Restart</button>
+                    <button class="rounded btn btn-primary" style="font-size:20px; padding:10px; width:200px; margin:0 20px;">Save</button>
+                </form> 
+            </div>
+            
         </div>
     </div>
-</div>    
 
+        <!-- peta versi 1 -->
+        {{-- </div>
+        <div class="container d-flex justify-content-center mt-4 ">
+            <div class="map-container">
+                <img src="{{ asset('img/kotaLama/MapBesarPolos.png')}}" class="city-map">
+                <div class="city" id="1" style="top: 150px; left: 250px;"></div>
+                <div class="city"id="2" style="top: 70px; left: 650px;"></div>
+                <div class="city" id="3"style="top: 450px; left: 520px;"> </div>
+                <div class="city" id="4"style="top: 365px; left: 780px;"></div>
+                <div class="city"id="5" style="top: 310px; left: 190px;"></div>
+                <div class="city" id="6"style="top: 500px; left: 200px;"></div>
+                <div class="city" id="7"style="top: 150px; left: 870px;"></div>
+                <div class="city" id="8"style="top: 250px; left: 480px;"></div>
+                <div class="city" id="9"style="top: 550px; left: 950px;"></div>
+            </div>    
+        </div>
+        <div class="container d-flex justify-content-center mt-4">
+            <form action=""method="POST" class="d-inline">
+                @csrf
+                <button class="rounded btn btn-primary mb-4 " style="font-size:20px; padding:20px; width: 250px;">Restart</button>
+                <button class="rounded btn btn-primary mb-4" style="font-size:20px; padding:20px; width:250px">Save</button>
+            </form>               
+        </div> --}}
 
-    
+        <!-- Modal -->
+        <div class="modal fade" id="moveModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Move City</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p id="modalMessage"></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="confirmMove">Confirm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script>
         let mapsData = @json($maps->pluck('city_id'));
@@ -196,7 +220,8 @@
         destinationList.forEach(function(cityId) {
             $("#" + cityId).addClass("choice").on('click', function() {
                 let cityName = $("#" + cityId).text();
-                $('#modalMessage').text('Do you want to move to city ' + cityName + '?');
+                $('#modalMessage').text('Do you want to move to ' + cityName + '?');
+                // $('#modalMessage').text("*if close button doesn't work, click anywhere outside of the box");
                 $('#moveModal').modal('show');
                 $('#confirmMove').off('click').on('click', function() {
                     $.ajax({
@@ -223,7 +248,5 @@
             });
         });
     </script>
-
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
 </x-layout>
